9e9e821a004e16dab1178369af3ca10bb5d2e64a
Manel Pavon <manelpavon10@gmail.com>
2017-02-03 17:49:31 +0100

    week3 day5 half done

---
+++ week3/day5/bank/src/main/java/bank/domain/Customer.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2016-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package bank.domain;
+
+import java.util.UUID;
+
+/**
+ * @author Sam Brannen
+ */
+public class Customer {
+
+	private final String id = UUID.randomUUID().toString();
+	private final String name;
+	private Account account;
+
+	public Customer(String name) {
+		this.name = name;
+	}
+
+	public String getId() {
+		return this.id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public Account getAccount() {
+		return this.account;
+	}
+
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + this.id + ", name=" + this.name + ", account=" + this.account + "]";
+	}
+
+}
